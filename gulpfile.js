058f5cbe9bea9e7b87c174a16e8cd1578025bcf9
Timo Sand <timo.j.sand@gmail.com>
2014-04-15 22:27:44 +0300

    Converted to jade

--- gulpfile.js
+++ gulpfile.js
@@ -28,35 +28,36 @@ var gutil      = require('gulp-util'),
 	chalk      = require('chalk'),
 	args       = require('yargs').argv,
 	browserSync = require('browser-sync'),
-	runSequence = require('run-sequence');
+	runSequence = require('run-sequence'),
+    jade        = require('gulp-jade');
 
-	require('gulp-grunt')(gulp); 
-	
+	require('gulp-grunt')(gulp);
+
 
 	var	settings = {
 		livereloadPort: 35729,
 		serverPort: 9000,
 		src: {
 			app: 'app/',
-			css: 'app/css/',			
-			js: 'app/js/',				
-			templates: 'app/templates/',				
+			css: 'app/css/',
+			js: 'app/js/',
+			templates: 'app/templates/',
 			images: 'app/images/',
 			fonts: 'app/fonts/',
-			bower: 'bower_components/'			
+			bower: 'bower_components/'
 		},
 		build: {
 			app: 'build/',
 			css: 'build/css/',
-			js: 'build/js/',				
-			templates: 'build/templates/',				
+			js: 'build/js/',
+			templates: 'build/templates/',
 			images: 'build/images/',
 			fonts: 'build/fonts/',
-			bower: 'bower_components/'			
+			bower: 'bower_components/'
 		},
-		scss: 'scss/'  
+		scss: 'scss/'
 	};
- 
+
  	// jsHint Options.
  	var hintOptions = JSON.parse(fs.readFileSync('.jshintrc', 'utf8'));
 
@@ -69,7 +70,7 @@ var gutil      = require('gulp-util'),
 ============================================================*/
 
 	gulp.task('local:server', function () {
-	
+
 		var middleware = [
 			require('connect-livereload')({ port: settings.livereloadPort }),
 			connect.static(settings.build.app),
@@ -119,31 +120,31 @@ var gutil      = require('gulp-util'),
 	gulp.task('concat', ['concat:js', 'concat:css']);
 
 	gulp.task('concat:js', ['js:hint'], function () {
-	
+
 		console.log('-------------------------------------------------- CONCAT :js');
 	  	gulp.src([settings.src.js + 'libs/*.js', settings.src.js + 'app.js', settings.src.js + '*.js', settings.src.js + '**/*.js'])
 		    .pipe(concat("all.js"))
 		    // .pipe(gulpif(isProduction, rename('all.min.js')))
 		    .pipe(gulpif(isProduction, uglify()))
-		    .pipe(gulp.dest(settings.build.js))		    
-		    .pipe(refresh(livereload));        
+		    .pipe(gulp.dest(settings.build.js))
+		    .pipe(refresh(livereload));
 
 		gulp.run('js:hint');
 	});
 
 	gulp.task('convert:scss', function () {
        console.log('-------------------------------------------------- COVERT - scss');
-       
+
         var stream = gulp.src(settings.src.css + 'application.scss')
            .pipe(sass({includePaths: [settings.src.css]}))
            .pipe(gulp.dest(settings.scss))
            .pipe(refresh(livereload));
-        return stream;           
+        return stream;
     });
 
 	gulp.task('concat:css', ['convert:scss'], function () {
 
-		console.log('-------------------------------------------------- CONCAT :css ');	  		  		
+		console.log('-------------------------------------------------- CONCAT :css ');
 	  	gulp.src([settings.src.css + 'fonts.css', settings.scss + 'application.css', settings.src.css + '*.css'])
 		    .pipe(concat('styles.css'))
 		    // .pipe(gulpif(isProduction, rename('styles.min.css')))
@@ -152,7 +153,7 @@ var gutil      = require('gulp-util'),
 		    .pipe(refresh(livereload));
 	});
 
-	
+
 
 
 
@@ -175,22 +176,24 @@ var gutil      = require('gulp-util'),
 =                           Copy                            =
 ============================================================*/
 
-	gulp.task('copy', ['copy:html', 'copy:images', 'copy:fonts', 'copy:html:root']);
+	gulp.task('copy', ['copy:jade', 'copy:images', 'copy:fonts', 'copy:jade:root']);
 
-	
-	gulp.task('copy:html', function () {
-		
-		console.log('-------------------------------------------------- COPY :html');
-		gulp.src([settings.src.templates + '*.html', settings.src.templates + '**/*.html'])
+
+	gulp.task('copy:jade', function () {
+
+		console.log('-------------------------------------------------- COPY :jade');
+		gulp.src([settings.src.templates + '*.jade', settings.src.templates + '**/*.jade'])
+            .pipe(jade())
 			.pipe(gulpif(isProduction, minifyHTML({comments: false, quotes: true, spare:true, empty: true, cdata:true})))
 			.pipe(gulp.dest(settings.build.templates))
 			.pipe(refresh(livereload));
 	});
 
-	gulp.task('copy:html:root', function() {
-		
-		console.log('-------------------------------------------------- COPY :html:root');
-		gulp.src(settings.src.app + '*.html')
+	gulp.task('copy:jade:root', function() {
+
+		console.log('-------------------------------------------------- COPY :jade:root');
+		gulp.src(settings.src.app + '*.jade')
+            .pipe(jade())
 			.pipe(gulpif(isProduction, minifyHTML({comments: false, quotes: true, spare:true, empty: true, cdata:true})))
 			.pipe(gulp.dest(settings.build.app))
 			.pipe(refresh(livereload));
@@ -202,7 +205,7 @@ var gutil      = require('gulp-util'),
 		gulp.src([settings.src.images + '*.*', settings.src.images + '**/*.*'])
 			.pipe(gulp.dest(settings.build.images));
 	});
- 
+
  	gulp.task('copy:fonts', function() {
 
  		console.log('-------------------------------------------------- COPY :fonts');
@@ -222,16 +225,16 @@ var gutil      = require('gulp-util'),
 =========================================================================================================*/
 
 	gulp.task('watch', function () {
-		
+
 		console.log('watching all the files.....');
 
 		var cssFiles   = gulp.watch([settings.src.css + '*.css',  settings.src.css + '**/*.css'],  ['concat:css']);
-		
+
 		var scssFiles  = gulp.watch([settings.src.css + '*.scss', settings.src.css + '**/*.scss'], ['concat:css']);
 
         var jsFiles    = gulp.watch([settings.src.js + '*.js',    settings.src.js + '**/*.js'],    ['concat:js']);
 
-        var indexFile  = gulp.watch([settings.src.app + '*.html'], ['copy:html:root']);
+        var indexFile  = gulp.watch([settings.src.app + '*.jade'], ['copy:jade:root']);
 
 		var imageFiles = gulp.watch([settings.src.images + '*.*', settings.src.images + '**/*.*'], ['copy:images']);
 
@@ -239,7 +242,7 @@ var gutil      = require('gulp-util'),
 
 		var libFiles   = gulp.watch([settings.src.bower + '*.js', settings.src.bower + '**/*.js'], ['grunt-bower']);
 
-		var templateFiles = gulp.watch([settings.src.templates + '*.html', settings.src.templates + '**/*.html'], ['copy:html']);
+		var templateFiles = gulp.watch([settings.src.templates + '*.jade', settings.src.templates + '**/*.jade'], ['copy:jade']);
 
 
 		// Just to add log messages on Terminal, in case any file is changed
@@ -263,8 +266,8 @@ var gutil      = require('gulp-util'),
 ============================================================*/
 
 	gulp.task('clean', function () {
-	
-		// gulp.run('clean:css', 'clean:js', 'clean:html', 'clean:images', 'clean:fonts');
+
+		// gulp.run('clean:css', 'clean:js', 'clean:jade', 'clean:images', 'clean:fonts');
 	    gulp.src([settings.build.app], {read: false})
 	        .pipe(clean({force: true}));
 	});
@@ -283,9 +286,9 @@ var gutil      = require('gulp-util'),
 	        .pipe(clean({force: true}));
 	});
 
-	gulp.task('clean:html', function () {
+	gulp.task('clean:jade', function () {
 
-		console.log('-------------------------------------------------- CLEAN :html');
+		console.log('-------------------------------------------------- CLEAN :jade');
 	    gulp.src([settings.build.templates], {read: false})
 	        .pipe(clean({force: true}));
 	});
@@ -319,12 +322,12 @@ var gutil      = require('gulp-util'),
 	gulp.task('zip', function () {
 	    gulp.src([settings.build.app + '*', settings.build.app + '**/*'])
 	        .pipe(zip('build-' + new Date() + '.zip'))
-	        .pipe(gulp.dest('./zip/'));	 
+	        .pipe(gulp.dest('./zip/'));
 
-	    setTimeout(function(){	    	
-	    	gulp.run('clean:zip');   
+	    setTimeout(function(){
+	    	gulp.run('clean:zip');
 	    }, 500); // wait for file creation
-		    
+
 	});
 
 
@@ -360,7 +363,7 @@ var gutil      = require('gulp-util'),
 ============================================================*/
 
 	gulp.task('sync', function() {
-	    browserSync.init([settings.build.app + 'index.html', settings.build + 'templates/*.html', settings.build.css + '*.css', settings.build.js + '*.js'], {
+	    browserSync.init([settings.build.app + 'index.jade', settings.build + 'templates/*.jade', settings.build.css + '*.css', settings.build.js + '*.js'], {
 	         proxy: {
 	            host: '127.0.0.1',
 	            port: settings.serverPort
