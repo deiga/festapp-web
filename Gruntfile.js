8ba3fc8cfe25256955136fdfb7c03856d2649ff4
Timo Sand <timo.j.sand@gmail.com>
2014-04-15 22:02:13 +0300

    Updated dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,62 @@
 
 module.exports = function (grunt) {
-	
-	'use strict';
+
+    'use strict';
 
     grunt.initConfig({
-  		pkg: grunt.file.readJSON('package.json'),
-	  		
-		shell: {
-	        
-	        bower_install: {
-	        	command: 'bower install'
-	        }
-	    },
-		
-		uglify: {
-	        files: { 
-	            src: 'build/js/_bower.js',  // source files mask
-	            dest: 'build/js/',    // destination folder
-	            expand: true,    // allow dynamic building
-	            flatten: true,   // remove all unnecessary nesting
-	            ext: '.min.js'   // replace .js to .min.js
-	        }
-	    },
+        pkg: grunt.file.readJSON('package.json'),
 
-	    bower_concat: {
-		  all: {
-		    dest: 'build/js/_bower.js',
-		    include: [
-		        "angular",
-			    "angular-resource",
-			    "angular-cookies",
-			    "angular-sanitize",
-			    "angular-route",
-			    "angular-animate",
-			    "zepto",
-			    "modernizr"
-		    ],
-		    dependencies: {
-				
-				'angular-resource': 'angular',
-				'angular-cookies': 'angular',
-				'angular-sanitize': 'angular',
-				'angular-route': 'angular',
-				'angular-animate': 'angular'		        
-		    },
-		    bowerOptions: {
-		      relative: true
-		    }
-		  }
-		}
-	});
+        shell: {
 
-	// load plugins	
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-bower-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+            bower_install: {
+                command: 'bower install'
+            }
+        },
 
-	grunt.registerTask('bower', ['bower_concat','uglify']);
-	grunt.registerTask('bower_install', ['shell:bower']);
+        uglify: {
+            files: {
+                src: 'build/js/_bower.js',  // source files mask
+                dest: 'build/js/',    // destination folder
+                expand: true,    // allow dynamic building
+                flatten: true,   // remove all unnecessary nesting
+                ext: '.min.js'   // replace .js to .min.js
+            }
+        },
+
+        bower_concat: {
+          all: {
+            dest: 'build/js/_bower.js',
+            include: [
+                "angular",
+                "angular-resource",
+                "angular-cookies",
+                "angular-sanitize",
+                "angular-route",
+                "angular-animate",
+                "zepto",
+                "modernizr",
+                "semantic-ui"
+            ],
+            dependencies: {
+
+                'angular-resource': 'angular',
+                'angular-cookies': 'angular',
+                'angular-sanitize': 'angular',
+                'angular-route': 'angular',
+                'angular-animate': 'angular'
+            },
+            bowerOptions: {
+              relative: true
+            }
+          }
+        }
+    });
+
+    // load plugins
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('bower', ['bower_concat','uglify']);
+    grunt.registerTask('bower_install', ['shell:bower']);
 };
