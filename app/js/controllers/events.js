8fda0668ea570ba82ac0b1f4ad167c345f9a8816
Timo Sand <timo.j.sand@gmail.com>
2014-04-20 04:00:59 +0300

    Added listing of festival info, upcoming events and stages

--- app/js/controllers/events.js
+++ app/js/controllers/events.js
@@ -10,10 +10,25 @@ app.controller('EventsCtrl', ['$scope', 'Restangular', function ($scope, Restang
 
 	console.log('Controller ===  EventsCtrl');
 
+    Restangular.all('festival').getList().then(function (festivals) {
+        $scope.festival = festivals[0];
+    });
+
     var baseEvents = Restangular.all('events');
 
-    baseEvents.getList().then(function (events) {
+    baseEvents.getList({sort: 'start_time'}).then(function (events) {
         $scope.allEvents = events;
+        $scope.upcomingEvents = events.filter(function (event) {
+            return new Date(event.start_time) >= Date.now();
+        });
+    });
+
+    var baseLocations = Restangular.all('locations');
+
+    baseLocations.getList().then(function (locations) {
+        $scope.stages = locations.filter(function (location) {
+            return location.type === 'STAGE';
+        });
     });
 
 }]);
